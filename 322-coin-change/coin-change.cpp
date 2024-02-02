29c0736274c8f48c9bd12107a0d710da3da28069
Bhavani <96565325+Shilpabhavani1@users.noreply.github.com>
2024-02-02 23:28:21 +0530

    Time: 30 ms (88.48%) | Memory: 12.7 MB (88.14%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -5,7 +5,7 @@ public:
     ll Coinchange(vector<int>& coins, ll amount , ll n)
     {
         if(amount==0)return 0;
-        if(amount<0)return INT_MAX;
+        // if(amount<0)return INT_MAX;
         if(dp[amount]!=-1)return dp[amount];
         ll ans=INT_MAX;
         for(int i=0;i<n;i++)
