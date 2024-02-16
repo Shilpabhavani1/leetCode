9e39a9dc95c2313b52aaad25c7d866bf137383b9
Bhavani <96565325+Shilpabhavani1@users.noreply.github.com>
2024-02-16 21:58:39 +0530

    Time: 429 ms (36.41%) | Memory: 37.6 MB (31.86%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.py
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.py
@@ -1,19 +1,31 @@
 class Solution:
     def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
-        freq = {}
+        d = {}
         for i in arr:
-            if i not in freq:
-                freq[i] = 1
+            if i not in d:
+                d[i] = 1
             else:
-                freq[i] += 1
-        freq = sorted(freq.items(), key=lambda x:x[1])
-        ans = []
-        for i in freq:
-            ans.append([i[0]]*i[1])
-        # print(ans)
-        secans = []
-        for i in ans:
-            for j in i:
-                secans.append(j)
-        # print(set(secans[k:]))
-        return len(set((secans[k:])))
+                d[i] += 1
+        l=[]
+        for i,j in d.items():
+            l.append((j,i))
+        l.sort()
+        l1=[]
+        for i in l:
+            l1.extend([i[1]]*i[0])
+        return len(set(l1[k:]))
+
+
+
+
+        # d = sorted(d.items(), key=lambda x:x[1])
+        # ans = []
+        # for i in d:
+        #     ans.append([i[0]]*i[1])
+        # # print(ans)
+        # r = []
+        # for i in ans:
+        #     for j in i:
+        #         r.append(j)
+        # # print(set(r[k:]))
+        # return len(set((r[k:])))
