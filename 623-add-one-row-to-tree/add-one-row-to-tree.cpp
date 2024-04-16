7cd0c3fe7b284aa65004801f843502e7360cc223
Bhavani <96565325+Shilpabhavani1@users.noreply.github.com>
2024-04-16 17:45:10 +0530

    Time: 12 ms (68.65%) | Memory: 24.4 MB (49.35%) - LeetSync

---
+++ 623-add-one-row-to-tree/add-one-row-to-tree.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
+        if(depth == 1){
+            TreeNode* newhead = new TreeNode(val);
+            newhead->left = root;
+            return newhead;
+        }
+        int lvl = 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            lvl++;
+            if(lvl == depth-1) break;
+            int n=q.size();
+            while(n--){
+                auto it = q.front();
+                q.pop();
+                if(it->left) q.push(it->left);
+                if(it->right) q.push(it->right);
+            }
+        }
+        int n = q.size();
+        while(n--){
+            auto node = q.front();
+            q.pop();
+            TreeNode*temp = new TreeNode(val);
+            TreeNode*temp1 = new TreeNode(val);
+            temp->left=node->left;
+            temp1->right=node->right;
+            node->left=temp;
+            node->right=temp1;
+        }
+        return root;
+    }
+};
