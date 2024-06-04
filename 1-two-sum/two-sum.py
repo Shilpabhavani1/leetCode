2d6ca42d1bca92944fcd7fecc61156a23c03c7e7
Bhavani <96565325+Shilpabhavani1@users.noreply.github.com>
2024-06-04 10:25:40 +0530

    Time: 53 ms (87.20%) | Memory: 17.7 MB (38.50%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,57 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        l=[]
-        n=len(nums)
-        for i in range(n):
-            for j in range(i+1,n):
-                if nums[i]+nums[j]==target:
-                    l.append(i)
-                    l.append(j)
-        return l
-
-
-
-
-
-
-
-
-
-
-
-
-        # l=[]
-        # n=len(nums)
-        # for i in range(n):
-        #     for j in range(i+1,n):
-        #         if nums[i]+nums[j]==target:
-        #             l.append(i)
-        #             l.append(j)
-        # return l
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # for i in range(len(nums)-1):
+        prevmap={} #hash map val:ind
+        for i,n in enumerate(nums):
+            diff=target-n
+            if diff in prevmap:
+                return prevmap[diff],i
+            prevmap[n]=i
+        return
+        # for i in range(len(nums)+1):
         #     if nums[i]+nums[i+1]==target:
-        #         return (i,i+1)
-        # return []
-        # l=[]
-        # for i in range(len(nums)):
-        #     for j in range(i+1,len(nums)):
-        #         if nums[i]+nums[j]==target:
-        #             l.append(i)
-        #             l.append(j)
-        #             break
-        # return l
+        #         return i,i+1
         
