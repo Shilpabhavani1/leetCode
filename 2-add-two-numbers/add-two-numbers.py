f2efb31deadef6e0ce24b32c4e4234492fa6b451
Bhavani <96565325+Shilpabhavani1@users.noreply.github.com>
2024-07-24 05:07:48 +0530

    Time: 54 ms (62.75%) | Memory: 16.6 MB (65.71%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -1,24 +1,18 @@
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        dummyHead = ListNode(0)
-        tail = dummyHead
-        carry = 0
-
-        while l1 is not None or l2 is not None or carry != 0:
-            digit1 = l1.val if l1 is not None else 0
-            digit2 = l2.val if l2 is not None else 0
-
-            sum = digit1 + digit2 + carry
-            digit = sum % 10
-            carry = sum // 10
-
-            newNode = ListNode(digit)
-            tail.next = newNode
-            tail = tail.next
-
-            l1 = l1.next if l1 is not None else None
-            l2 = l2.next if l2 is not None else None
-
-        result = dummyHead.next
-        dummyHead.next = None
-        return result
+        carry=0
+        temp=ListNode(0)
+        curr=temp
+        while l1 or l2 or carry!=0:
+            v1=l1.val if l1 is not None else 0
+            v2=l2.val if l2 is not None else 0
+            total=v1+v2+carry
+            carry=total//10
+            new_node=ListNode(total%10)
+            curr.next=new_node
+            curr=curr.next
+            if l1 is not None:
+                l1=l1.next
+            if l2 is not None:
+                l2=l2.next
+        return temp.next
