97fc1d32de636a9666bf82a10103ea0d8dcd44ae
Bhavani <96565325+Shilpabhavani1@users.noreply.github.com>
2024-11-10 13:26:46 +0530

    Time: 38 ms (82.26%) | Memory: 25.8 MB (98.12%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -2,12 +2,85 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n=len(prices)
         buy=prices[0]
-        profit=0
-        for i in range(1,n):
+        p=0
+        for i in range(1,len(prices)):
             if buy>prices[i]:
                 buy=prices[i]
-            elif prices[i]-buy>profit:
-                profit=prices[i]-buy
-        return profit
+            elif prices[i]-buy>p:
+                p=prices[i]-buy
+        return p
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # l=len(prices)
+        # buy=prices[0]
+        # p=0
+        # for i in range(1,l):
+        #     if prices[i]<buy:
+        #         buy=prices[i]
+        #     elif prices[i]-buy>p:
+        #         p=prices[i]-buy
+        # return p
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # #Two pointers concept
+        # l,r=0,1
+        # maxprofit = 0
+        # while r < len(prices):
+        #     if prices[l] < prices[r]:
+        #         profit = prices[r] - prices[l]
+        #         maxprofit=max(maxprofit,profit)
+        #     else:
+        #         l = r
+        #     r = r + 1
+        # return maxprofit
+
+
+        # n=len(prices)
+        # buy=prices[0]
+        # profit=0
+        # for i in range(1,n):
+        #     if buy>prices[i]:
+        #         buy=prices[i]
+        #     elif prices[i]-buy>profit:
+        #         profit=prices[i]-buy
+        # return profit
 
         
